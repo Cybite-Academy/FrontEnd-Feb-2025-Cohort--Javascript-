373495c519910c0143815efb3026083435d95166
O'Neil Obidiaso <obidiasodumebi@gmail.com>
2025-03-13 12:37:34 +0100

    - JS at a glance, - Datatype [string, number, boolean] - typeof operator - variable I

---
+++ script.js
@@ -0,0 +1,46 @@
+// window.alert("Hello, people!");
+// document.write("<h1 style='color: blue;'>Hello World</h1>");
+// console.log("Hello, people!");
+
+/*
+//===== strings
+console.log("O'neil");
+console.log("odoh9");
+console.log("9");
+// Mrs Lawal
+console.log("omo15");
+console.log("most beeautiful");
+ 
+// Collins
+console.log("ODOH");
+console.log("COLLINS");
+
+//=====numbers
+console.log(9);
+console.log(100000);
+// Mrs Lawal
+
+// Collins
+console.log(08043)
+console.log(08044);
+*/
+
+// ==== Typeof
+// console.log(typeof("9"))
+// console.log(typeof(9))
+
+// console.log(9+9);
+// console.log(9*9000);
+
+// console.log('3+3');
+// console.log('9*9000');
+// console.log(100000 * 18);
+
+//===== Boolean
+// console.log(typeof(true));
+// console.log(typeof(false));
+// console.log(5 > 9);
+// console.log(10 > 9);
+// console.log(10 == 10);
+
+
---
+++ variables.js
@@ -0,0 +1,28 @@
+//======= variables
+// automatic assignment
+// x = 5;
+// y = "Bob";
+// student_id = 123;
+
+// console.log(x);
+// console.log(y);
+
+// explicit assignment
+// var assignment= 123;
+// let name1= "Jones Walter";
+// let a = 'paul';
+
+// const pi = 3.14;
+// console.log(name1);
+// console.log(pi);
+// console.log(A);
+
+
+let first_name = 'John';
+let last_name = 'Doe';
+let full_name = first_name + ' ' + last_name;
+console.log(first_name);
+console.log(last_name);
+console.log('my name is ',first_name + ' ' + last_name);
+
+console.log('fullname: ',full_name);
