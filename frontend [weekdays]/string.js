31ea705ecba2cc4b69509f2ae917f8fc1f5918c1
O'Neil Obidiaso <obidiasodumebi@gmail.com>
2025-03-19 11:56:36 +0100

    strings update

---
+++ frontend [weekdays]/string.js
@@ -0,0 +1,82 @@
+// let answer1 = "It's alright";
+// let answer2 = "He is called 'Johnny'";
+// let answer3 = 'He is called "Johnny"';
+// console.log(answer1, '\n', answer2, '\n', answer3);
+
+// let text = `He's often called "Johnny"
+// djhnidfgnjgnjr
+// djgbrgburgj
+
+// kfgii
+// 12345
+// `;
+// console.log(text);
+
+//string length
+// let message = "hello world12345";
+// console.log(message.length);
+
+// let name = "you are highered";
+// console.log(name.length);
+
+// let check = "lets be friends for now";
+// console.log(check.length);
+
+//escape sequence
+// let text = "We are the so-called \"Vikings\" from the north.";
+// console.log(text);
+
+// let text2 = "T\the character \n\v is call \n ed backslash.";
+// console.log(text2);
+
+// string methods
+/*
+//length
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
+console.log(length);
+
+let firstName = "O'Neil";
+let lastName = "Obidiaso";
+
+let firstName_len = firstName.length;
+let lastName_len= lastName.length;
+console.log(firstName_len == lastName_len);
+*/
+
+/*
+//uppercase and lowercase
+let text = "EARTH";
+let text_upper = text.toLowerCase();
+console.log(text_upper);
+
+let planet = "PLUTO";
+let planet_upper = planet.toLowerCase();
+console.log(planet_upper);
+*/
+
+/*
+//char at
+let text = "HELLO WORLD";
+let singleLetter = text.charAt(6);
+console.log(singleLetter);
+*/
+
+//concat
+// let text1 = "Hello";
+// let text2 = "World";
+// let text3 = text1 + " " + text2;
+// console.log(text3);
+
+
+// replace
+// let text = "Please visit Microsoft!";
+// console.log(text);
+
+//replace
+// let newText = text.replace("Microsoft", "Cybite");
+// console.log(newText);
+
+//match
+let text = "The rain in SPAIN stays mainly in the plain";
+console.log(text.match("ain"));
